b7684cee9573646ac0e4ee8c3e9516f73c52d6c0
Tola Dev <lonwtola@gmail.com>
2025-07-22 10:30:48 +0700

    Add player control and test room

---
+++ Assets/_EscapeTheRoom/Script/HideLockMouse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class HideLockMouse : MonoBehaviour
+{
+
+    public bool lockCursor = true;
+
+    void Update()
+    {
+
+        // pressing esc toggles between hide/show
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            lockCursor = !lockCursor;
+        }
+
+        Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !lockCursor;
+    }
+}
